de2df521481c8f292dfece3729483ff8599134cf
Abdul_Samad <abdul.samad@tothenew.com>
2024-05-19 13:47:39 +0530

    "Removed MapSearch and SelectDropdownPortalContainer components from App.js"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import SelectDropdownPortalContainer from './components/shared/reactSelectDropdown/dropdownPortal/DropdownPortal';
 import CustomToastContainer from './components/shared/toastContainer/ToastContainer';
 import Navigation from './navigation/Navigation';
-import MapSearch from './pages/MapSearch/MapSearch';
 
 function App() {
   return (
     <div >
-      {/* <MapSearch />
-      <SelectDropdownPortalContainer /> */}
       <Navigation />
       <CustomToastContainer/>
     </div>
